2937fd188282fc3cb1fd004b03a8989cc3e9b5cb
Sourin Mukherjee <98275372+i-am-faith@users.noreply.github.com>
2022-10-03 01:03:25 +0530

    Update FahrenheittoCelsius.cpp

--- MILESTONE 1/Conditilnal Loops/FahrenheittoCelsius.cpp
+++ MILESTONE 1/Conditilnal Loops/FahrenheittoCelsius.cpp
@@ -1,3 +1,4 @@
+/*******************************************************************************************************************************
 Fahrenheit to Celsius Table
 Send Feedback
 Given three values - Start Fahrenheit Value (S), End Fahrenheit value (E) and Step Size (W), you need to convert all Fahrenheit values from Start to End at the gap of W, into their corresponding Celsius values and print the table.
